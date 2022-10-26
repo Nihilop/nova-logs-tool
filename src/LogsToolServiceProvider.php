e3cbc9e3f07ca84eee98dfb5f72cce643e9ea046
Nihilo <40236901+Nihilop@users.noreply.github.com>
2022-10-26 16:56:06 +0200

    fix auth routes by middleware

--- src/LogsToolServiceProvider.php
+++ src/LogsToolServiceProvider.php
@@ -4,11 +4,28 @@ namespace Stepanenko3\LogsTool;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Http\Middleware\Authenticate;
+use Laravel\Nova\Events\ServingNova;
 use Stepanenko3\LogsTool\Http\Middleware\Authorize;
 use Laravel\Nova\Nova;
 
 class LogsToolServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->booted(function () {
+            $this->registerRoutes();
+        });
+
+        Nova::serving(function (ServingNova $event) {
+            //
+        });
+    }
     /**
      * Register any application services.
      *
@@ -16,7 +33,7 @@ class LogsToolServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->registerRoutes();
+        // $this->registerRoutes();
     }
 
     /**
@@ -27,7 +44,7 @@ class LogsToolServiceProvider extends ServiceProvider
     protected function registerRoutes()
     {
         // Register nova routes
-        Nova::router(['nova:api', Authorize::class], 'logs-tool')->group(function ($router) {
+        Nova::router(['nova', Authenticate::class, Authorize::class])->group(function ($router) {
             $path = 'logs';
             $router->get($path, fn () => inertia('NovaLogs', ['basePath' => $path]));
         });
@@ -36,7 +53,7 @@ class LogsToolServiceProvider extends ServiceProvider
             return;
         }
 
-        Route::middleware(['nova:api', Authorize::class])
+        Route::middleware(['nova', Authorize::class])
             ->prefix('nova-vendor/stepanenko3/logs-tool')
             ->group(__DIR__ . '/../routes/api.php');
     }
